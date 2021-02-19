85dc3838d2324f39f0c53a56c6a7eb4a6048d809
Khorf Zakaria <gz_khorf@esi.dz>
2021-02-19 20:00:49 +0100

    Added Web dependencies

--- src/routes/web.js
+++ src/routes/web.js
@@ -1,10 +1,11 @@
 import express from "express"
-
+import chatBotController from "../controllers/chatBotController.js"
 let router = express.Router()
 
 let initWebRoutes = (app) => {
  router.get('/', (req , res) => {res.render ("homepage.ejs")})
-
+ router.get('/webhook', chatBotController.getWebhook)
+ router.post('/webhook', chatBotController.postWebhook)
  return app.use('/',router)
 
 
