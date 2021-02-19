837b7b27628acb8feeca6d2fdf35ccfbb59858c4
Khorf Zakaria <gz_khorf@esi.dz>
2021-02-19 23:40:54 +0100

    Added FB  app

--- src/controllers/chatBotController.js
+++ src/controllers/chatBotController.js
@@ -1,28 +1,34 @@
 let postWebhook = (req,res) =>{
-    app.post('/webhook', (req, res) => {  
- 
-        let body = req.body;
+  app.post('/webhook', (req, res) => {  
+
+    // Parse the request body from the POST
+    let body = req.body;
+  
+    // Check the webhook event is from a Page subscription
+    if (body.object === 'page') {
+  
+      // Iterate over each entry - there may be multiple if batched
+      body.entry.forEach(function(entry) {
+
+        // Gets the body of the webhook event
+        let webhook_event = entry.messaging[0];
+        console.log(webhook_event);
       
-        // Checks this is an event from a page subscription
-        if (body.object === 'page') {
-      
-          // Iterates over each entry - there may be multiple if batched
-          body.entry.forEach(function(entry) {
-      
-            // Gets the message. entry.messaging is an array, but 
-            // will only ever contain one message, so we get index 0
-            let webhook_event = entry.messaging[0];
-            console.log(webhook_event);
-          });
-      
-          // Returns a '200 OK' response to all requests
-          res.status(200).send('EVENT_RECEIVED');
-        } else {
-          // Returns a '404 Not Found' if event is not from a page subscription
-          res.sendStatus(404);
-        }
+        // Get the sender PSID
+        let sender_psid = webhook_event.sender.id;
+        console.log('Sender PSID: ' + sender_psid);
       
       });
+  
+      // Return a '200 OK' response to all events
+      res.status(200).send('EVENT_RECEIVED');
+  
+    } else {
+      // Return a '404 Not Found' if event is not from a page subscription
+      res.sendStatus(404);
+    }
+  
+  });
 
 
 
@@ -52,9 +58,22 @@ let getWebhook = (req,res) =>{
     }
   }
 }
+function handleMessage(sender_psid, received_message) {
 
+}
+
+// Handles messaging_postbacks events
+function handlePostback(sender_psid, received_postback) {
+
+}
+
+// Sends response messages via the Send API
+function callSendAPI(sender_psid, response) {
+  
+}
 
 export default {
     getWebhook:getWebhook,
     postWebhook :postWebhook 
+
 }
